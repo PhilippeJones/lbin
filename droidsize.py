63569c1d52af01a334833b3a731cc00084459530
GaryRudolph <GaryRudolph@mac.com>
2013-08-29 15:49:40 -0700

    Updated to not do mdpi

--- droidsize.py
+++ droidsize.py
@@ -20,9 +20,8 @@ def usage():
     print ""
     print "  Options:"
     print "    -v, --verbose"
-    print "    -s, --size xxhdpi|xhdpi|hdpi|mdpi|ldpi"
+    print "    -s, --size xxhdpi|xhdpi|hdpi|mdpi"
     print "    -n, --name filename"
-    print "    -d, --drawable xxhdpi|xhdpi|hdpi|mdpi|ldpi"
 
 def main(argv):
     if len(argv) == 0:
@@ -30,7 +29,7 @@ def main(argv):
         sys.exit(2)
     
     try:
-        opts, args = getopt(argv, "hvs:n:d:", ["help", "verbose", "size=", "name=", "drawable="])
+        opts, args = getopt(argv, "hvs:n:", ["help", "verbose", "size=", "name="])
     except GetoptError:
         usage()
         sys.exit(2)
@@ -38,7 +37,6 @@ def main(argv):
     verbose = False
     size = "xxhdpi"
     name = None
-    drawable = "mdpi"
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):
@@ -50,8 +48,6 @@ def main(argv):
             size = arg
         elif opt in ("-n", "--name"):
             name = arg
-        elif opt in ("-d", "--drawable"):
-            drawable = arg
     
     if len(args) < 2:
         usage()
@@ -64,35 +60,24 @@ def main(argv):
         name = source.split("/")[-1]
     
     densities = {}
-    if size == "ldpi":
-        densities["ldpi"] = 0.75
-    elif size == "mdpi":
-        densities["ldpi"] = 0.75
+    if size == "mdpi":
         densities["mdpi"] = 1.0
     elif size == "hdpi":
-        densities["ldpi"] = 0.75
         densities["mdpi"] = 1.0
         densities["hdpi"] = 1.5
     elif size == "xhdpi":
-        densities["ldpi"] = 0.75
         densities["mdpi"] = 1.0
         densities["hdpi"] = 1.5
         densities["xhdpi"] = 2.0
     elif size == "xxhdpi":
-        densities["ldpi"] = 0.75
         densities["mdpi"] = 1.0
         densities["hdpi"] = 1.5
         densities["xhdpi"] = 2.0
         densities["xxhdpi"] = 3.0
     else:
-        print "%s is not a valid size, must be one of ldpi, mdpi, hdpi, xhdpi, xxhdpi." % (size)
+        print "%s is not a valid size, must be one of mdpi, hdpi, xhdpi, xxhdpi." % (size)
         print ""
         usage()
-        sys.exit(2)
-    
-    if densities[drawable] is None:
-        print "%s is not a valid drawable, must be one of ldpi, mdpi, hdpi, xhdpi, xxhdpi." % (drawable)
-        print ""
         sys.exit(2)
     
     if not os.path.isfile(source):
@@ -143,17 +128,6 @@ def main(argv):
                 print "Could not resize %s to %s" % (source, file_path)
                 print errors
                 sys.exit(2)
-    
-    drawable_path = "%s/drawable" % (resources);
-    if not os.path.isdir(drawable_path):
-        if verbose:
-            print "Creating directory %s" % (drawable_path)
-        os.makedirs(drawable_path)
-    drawable_source_path = "%s/drawable-%s/%s" % (resources, drawable, name)
-    drawable_file_path = "%s/%s" % (drawable_path, name)
-    if verbose:
-        print "Copying %s to %s" % (drawable_source_path, drawable_file_path)
-    shutil.copyfile(drawable_source_path, drawable_file_path)
     
     sys.exit(0)
     
